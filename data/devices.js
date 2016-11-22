d47939eff749de21447e3b1af1b5d809f94a07b5
Ryan Parsley <rmparsley@gmail.com>
2016-11-21 22:14:44 -0500

    add fake devices

---
+++ data/devices.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var fakeDay = new Date('November 20, 2016 03:24:00');
+
+module.exports = [
+  {
+    "lightbulb": {
+      "status": "on",
+      "type": "led",
+      "last-change: fakeDay
+    },
+    "lightbulb2": {
+      "status": "on",
+      "type": "led",
+      "last-change: fakeDay
+    },
+    "motor": {
+      "status": "on",
+      "type": "motor",
+      "last-change: fakeDay
+    }
+  }
+];
