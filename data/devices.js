2f17df3b5a6041c261a5e5693fe8f6d0b9ab7d48
Ryan Parsley <rmparsley@gmail.com>
2016-11-21 23:55:46 -0500

    fake lightbulb

--- data/devices.js
+++ data/devices.js
@@ -4,20 +4,19 @@ var fakeDay = new Date('November 20, 2016 03:24:00');
 
 module.exports = [
   {
-    "lightbulb": {
-      "status": "on",
-      "type": "led",
-      "last-change": fakeDay
-    },
-    "lightbulb2": {
-      "status": "on",
-      "type": "led",
-      "last-change": fakeDay
-    },
-    "motor": {
-      "status": "on",
-      "type": "motor",
-      "last-change": fakeDay
-    }
+    "name": "lightbulb",
+    "status": "on",
+    "type": "led",
+    "last-change": fakeDay
+  },{
+    "name": "lightbulb2",
+    "status": "on",
+    "type": "led",
+    "last-change": fakeDay
+  },{
+    "name": "motor",
+    "status": "on",
+    "type": "motor",
+    "last-change": fakeDay
   }
 ];
